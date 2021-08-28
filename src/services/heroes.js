979d38711dfeef93ea92358d8506368db68a99fe
unknown <ezegonzalez912@gmail.com>
2021-08-28 16:24:21 -0300

    remove env

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const heroesByName = (name) => {
 
-    const accessToken = process.env.REACT_APP_ACCESS_TOKEN
+    const accessToken = 379932106978719 //Deberia ser un .env
 
     const api_URL = `https://superheroapi.com/api.php/${accessToken}`;
 
