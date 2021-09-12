978bc616e81c7633b61dec43eb07396d27a916aa
unknown <ezegonzalez912@gmail.com>
2021-09-11 23:33:12 -0300

    fix login

--- src/services/heroes.js
+++ src/services/heroes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const heroesByName = (name) => {
 
-    const accessToken = 379932106978719 //Deberia ser un .env
+    const accessToken = process.env.REACT_APP_ACCESS_TOKEN
 
     const api_URL = `https://superheroapi.com/api.php/${accessToken}`;
 
